f1c83f19511483ab2ecbd7e92e27fb9859d8067e
Nayana <Nayana>
2016-04-07 18:49:33 +0800

    Updated version by Hx

--- estimote-sticker/estimotescan.js
+++ estimote-sticker/estimotescan.js
@@ -3,35 +3,36 @@ var mqtt = require('mqtt');
 var moment = require('moment');
 var Struct = require('struct');
 var fs = require('fs');
+var uuid = 0;
 
-//check for fileinput else exit process
-if (process.argv.length < 3){
+// check for file input else exit process
+if (process.argv.length < 3) {
 	console.log(process.argv[1]);
 	process.exit(1);
-}
+} // end if 
 
+configFile =  process.argv[2];
 
-configurationFile =  process.argv[2];
+try {
+	var configuration = JSON.parse(fs.readFileSync(configFile));
+} // end try
+catch (err) {
+	if (err.code !== 'ENOENT') {
+		console.log(err);
+	} // end if 
+	else {
+		console.log("ERROR: File does not exist");
+	} // end else
+} // end catch
 
-try{
-var configuration = JSON.parse(fs.readFileSync(configurationFile));
-} catch(err){
-	if (err.code!== 'ENOENT'){
-	console.log(err);
-	
-	}
-	else{
-	console.log("file does not exist");
-	}
-}
-//############
-//ConfigFile PARAM
+// ############
+// ConfigFile PARAM
 var nodeId = configuration["generic.node-id"];
 var configVer = configuration.configVer;
 var modalityMap = configuration.modalityMap;
 
-//###############
-//MQTT PARAMS
+// ###############
+// MQTT PARAMS
 var topic = configuration["mqtt.local.pubTopic"]+nodeId;
 var broker = configuration["mqtt.local.host"];
 var port = configuration["mqtt.local.port"];
@@ -40,8 +41,8 @@ var clientId = configuration["mqtt.local.options.clientId"];
 var KeepAlive = configuration["mqtt.local.options.KeepAlive"];
 var timeOut = configuration["mqtt.local.options.timeOut"];
 
-//##########
-//TEST
+// ##########
+// TEST
 
 console.log("LoadedData");
 console.log(configVer);
@@ -62,68 +63,46 @@ var publishOption = {qos:qos};
 
 //returns a sticker oject on discover in json
 EstimoteSticker.on('discover', function(estimoteSticker) {
-  
 	var estimoteData;
-	relevantdata(estimoteSticker,function(data){
-	
+	relevantdata(estimoteSticker,function(data) {	
 	estimoteData = data;
-
-
 });
-	
 	publish(estimoteData);
-	
 });
 
-// starts Scanning for stickers
+// starts scanning for stickers
 EstimoteSticker.startScanning();
 
-//Parse data and returns in callback
-function relevantdata(estimoteSticker, callback){
-console.log(estimoteSticker);
-var currentTime = moment().unix();
-//var header = {'timestamp':currentTime,'configVer': configVer,'modalityMap': modalityMap};
-var relevantJson ={'timestamp': currentTime,'nodeId': nodeId,'StickerId':estimoteSticker.uuid,'batteryLevel':estimoteSticker.batteryLevel,'power':estimoteSticker.power,'acceleration': estimoteSticker.acceleration};
+// Parse data and returns in callback
+function relevantdata(estimoteSticker, callback) {
+	console.log(estimoteSticker);
+	var currentTime = moment().unix();
+	// var header = {'timestamp':currentTime,'configVer': configVer,'modalityMap': modalityMap};
+	// var relevantJson ={'sensor_id':estimoteSticker.uuid,'battery_level':estimoteSticker.batteryLevel,'power':estimoteSticker.power,'acceleration': estimoteSticker.acceleration,'acc_x':estimoteSticker.acceleration['x']};
+	var relevantJson ={'uuid':estimoteSticker.uuid,'battery_level':estimoteSticker.batteryLevel,'power':estimoteSticker.power,'acc_x':estimoteSticker.acceleration['x'], 'acc_y':estimoteSticker.acceleration['y'], 'acc_z':estimoteSticker.acceleration['z']};
+	uuid = estimoteSticker.uuid;
+	// var payloadlength = relevantJson.length;
+	// header['payloadLen'] = payloadlength;
+	callback(relevantJson);
+} // end relevantdata
 
-//var payloadlength = relevantJson.length;
-//header['payloadLen'] = payloadlength;
+// Publish and forget
 
-
-
-
-
-callback(relevantJson);
-
-}
-
-//Publish and forget
-
-function publish(sticker){
-
-var client  = mqtt.connect(broker,clientOptions);
-
-
-//console.log(topicid)
-
-
-//send buffered data across
-client.publish(topic,JSON.stringify(sticker),publishOption,function(err,client){
-
-if(err===null){
-	console.log("Publish Successfull")
-}else{
-	console.log("Publish Gone Wrong")
-}
-console.log(client);
-
-});
-client.end(false,function(message){
-	console.log("client close");
-});
-
-}
-
-
-
-
-
+function publish(sticker) {
+	var client  = mqtt.connect(broker,clientOptions);
+	// console.log(topicid)
+	// send buffered data across
+	topic = configuration["mqtt.local.pubTopic"]+uuid;
+	client.publish(topic,JSON.stringify(sticker),publishOption,function(err,client) {
+		if (err===null) {
+			console.log("Publish Successful")
+		} // end if 
+		else {
+			console.log("Publish Gone Wrong")
+		} // end else
+		console.log(client);
+	});
+	client.end(false,function(message) {
+		console.log("client close");
+	});
+} // end publish
